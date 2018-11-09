ce6f64ba5f1f824aae8b2eebdce2708a08ac51c8
Serban Bondor <serbanbondor@yahoo.com>
2018-11-09 23:57:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const findCity = () => {
+    let cityName = document.getElementById("city-name").value;
+    let coordonates = document.getElementById("coordonates");
+    let myRequest = new XMLHttpRequest();
+    myRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=91f14a0652b82e7fac8c4203ddd69dc9');
+    myRequest.onload = function() {
+        let myData = JSON.parse(myRequest.responseText);
+        coordonates.innerHTML = "<p>Temperature: " + Math.floor(myData.main.temp) + "°C </p>";
+    };
+    myRequest.send();
+}
